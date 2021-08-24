492b6e2e0c021eaf81b68296db6af362ca5ccd18
Alan Arriaga <A14N.arriaga@gmail.com>
2021-08-24 00:31:00 -0500

    Canvas implementation

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,40 @@
-import React from "react"
+import { useEffect } from "react"
+
+const reloadGame = () => {
+  console.log("fotograma")
+}
+
+const loadGame = () => {
+  const canvas = document.getElementById("canvas-game") as HTMLCanvasElement
+  if (canvas !== null) {
+    const fps = 30
+    // const context = canvas.getContext("2D")
+    const canvasW = 944
+    const canvasH = 432
+    // const rows = 10
+    // const cols = 10
+    // const cellW = Math.floor(canvasW / rows)
+    // const cellH = Math.floor(canvasH / cols)
+    // const aliveColor = "#FFFFFF"
+    // const deadColor = "#000000"
+    canvas.width = canvasW
+    canvas.height = canvasH
+    setInterval(() => reloadGame(), 1000 / fps)
+  }
+}
 
 const Game = () => {
+  useEffect(() => {
+    loadGame()
+  }, [])
+
   return (
     <main>
-      <section className="container"></section>
+      <section className="container">
+        <div className="canvas-wrapper">
+          <canvas id="canvas-game"></canvas>
+        </div>
+      </section>
     </main>
   )
 }
