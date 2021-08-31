c0303ad4ab35675a223a66d7da943d123ab515f3
Alan Arriaga <A14N.arriaga@gmail.com>
2021-08-31 01:03:51 -0500

    Cells implement

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,38 +1,86 @@
 import { useEffect } from "react"
 
-const reloadGame = () => {
+const rows = 70
+const cols = 70
+let board: Array<Array<Cell>>
+
+class Cell {
+  x: number
+  y: number
+  state: number
+  nextState: number
+  neighbors: Array<Cell>
+  constructor(x: number, y: number, state: number) {
+    this.x = x
+    this.y = y
+    this.state = state
+    this.nextState = state
+    this.neighbors = new Array<Cell>(8)
+  }
+  getNeighbors = () => {
+    const moveAxis = [-1, 0, 1]
+    for (const dx of moveAxis) {
+      for (const dy of moveAxis) {
+        if (dx === 0 && dy === 0) continue
+        const neighborX = (this.x + dx + rows) % rows
+        const neighborY = (this.y + dy + cols) % cols
+        this.neighbors.push(board[neighborX]?.[neighborY])
+      }
+    }
+  }
+  paint = () => {}
+}
+
+const cleanBoard = () => {}
+
+const paintBoard = () => {
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) board[x][y].paint()
+  }
+}
+
+const reloadBoard = () => {
+  cleanBoard()
+  paintBoard()
   console.log("fotograma")
 }
 
-const loadGame = () => {
-  const canvas = document.getElementById("canvas-game") as HTMLCanvasElement
-  if (canvas !== null) {
-    const fps = 30
-    // const context = canvas.getContext("2D")
-    const canvasW = 944
-    const canvasH = 464
-    // const rows = 10
-    // const cols = 10
-    // const cellW = Math.floor(canvasW / rows)
-    // const cellH = Math.floor(canvasH / cols)
-    // const aliveColor = "#FFFFFF"
-    // const deadColor = "#000000"
-    canvas.width = canvasW
-    canvas.height = canvasH
-    setInterval(() => reloadGame(), 1000 / fps)
+const loadBoard = () => {
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) {
+      const state = Math.floor(Math.random() * 2)
+      board[x][y] = new Cell(x, y, state)
+    }
   }
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) board[x][y].getNeighbors()
+  }
+  console.log(board)
+}
+
+const loadGame = ({ fps }: { fps: number }) => {
+  board = Array.from(new Array(rows)).map(row => new Array(cols)) as Array<Array<Cell>>
+  loadBoard()
+  setInterval(() => reloadBoard(), 1000 / fps)
 }
 
 const Game = () => {
   useEffect(() => {
-    loadGame()
+    loadGame({ fps: 30 })
   }, [])
+
+  const prueba = []
+  for (let i = 0; i < 54 * 26; i++) {
+    i % 2 === 0
+      ? prueba.push(<div className="alive"></div>)
+      : prueba.push(<div className="dead"></div>)
+  }
 
   return (
     <main>
       <section className="container">
-        <div className="canvas-wrapper">
-          <canvas id="canvas-game"></canvas>
+        <div className="game-wrapper">
+          <div className="game">{prueba}</div>
         </div>
       </section>
     </main>
